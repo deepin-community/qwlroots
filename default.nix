c9c29af337981ae9ed5576b510f3f5b3ca2f769c
justforlxz <justforlxz@gmail.com>
2024-08-09 13:31:24 +0800

    chore: bump version to 0.2.0-alpha2+git+20240808-deepin
    as title
    
    Log:

--- default.nix
+++ default.nix
@@ -5,32 +5,20 @@ let
     (builtins.substring 4 2 longDate)
     (builtins.substring 6 2 longDate)
   ]);
-
-  wlroots_master = pkgs.wlroots_0_17.overrideAttrs (
-    old: {
-      version = "0.18.0";
-      src = wlroots_master_src;
-      patches = [];
-      buildInputs = old.buildInputs ++ (with pkgs; [ 
-        lcms
-      ]);
-    }
-  );
-
 in
 rec {
   qwlroots-qt6 = pkgs.qt6.callPackage ./nix {
     inherit nix-filter;
     wlroots = pkgs.wlroots_0_17;
   };
-  
+
   qwlroots-qt5 = pkgs.libsForQt5.callPackage ./nix {
     inherit nix-filter;
     wlroots = pkgs.wlroots_0_17;
   };
 
-  qwlroots-qt6-wlroots-master = qwlroots-qt6.override {
-    wlroots = wlroots_master;
+  qwlroots-qt6-wlroots-18 = qwlroots-qt6.override {
+    wlroots = pkgs.wlroots_0_18;
   };
 
   qwlroots-qt6-dbg = qwlroots-qt6.override {
--- src/interfaces/qwkeyboardinterface.h
+++ src/interfaces/qwkeyboardinterface.h
@@ -5,36 +5,12 @@
 
 #include <qwinterface.h>
 
-struct wlr_keyboard;
-struct wlr_keyboard_impl;
-
 QW_BEGIN_NAMESPACE
 
-class QW_EXPORT QWKeyboardInterface : public QWInterface
-{
-    friend class QWKeyboard;
-public:
-    virtual ~QWKeyboardInterface();
+extern "C" {
+#include <wlr/interfaces/wlr_keyboard.h>
+}
 
-    virtual const char* name() = 0;
-
-    virtual void ledUpdate(uint32_t leds) const;
-
-    inline wlr_keyboard *handle() const {
-        return QWInterface::handle<wlr_keyboard>();
-    }
-    inline wlr_keyboard_impl *impl() const {
-        return QWInterface::impl<wlr_keyboard_impl>();
-    }
-    static QWKeyboardInterface *get(wlr_keyboard *handle);
-
-protected:
-    template<class T>
-    inline void init() {
-        init(getFuncMagicKey<T>(&T::ledUpdate));
-    }
-
-    virtual void init(FuncMagicKey funMagicKey);
-};
+QW_CLASS_INTERFACE(keyboard)
 
 QW_END_NAMESPACE
